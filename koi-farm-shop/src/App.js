512575fa7039c652963566760994ce54400c5feb
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-09-24 05:47:07 +0700

    Solve problem when merge code

--- koi-farm-shop/src/App.js
+++ koi-farm-shop/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import ProductList from "./components/productList/ProductList";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/productList" element={<ProductList />} />
+        {/* <Route path="/productList" element={<ProductList />} /> */}
       </Routes>
     </Router>
   );
