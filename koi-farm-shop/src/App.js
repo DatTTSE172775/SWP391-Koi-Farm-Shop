1ca6d274ec52220bf3b317c81c1968fc56b51437
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-09-24 06:58:27 +0700

    Add aboutPage

--- koi-farm-shop/src/App.js
+++ koi-farm-shop/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import About from "./components/about/About";
 import Blog from "./components/blog/Blog";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/homepage" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
       <Footer />
