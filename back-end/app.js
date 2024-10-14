c40150c031282f9466ff768ee6efc087fa3eecee
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-10-14 22:57:58 +0700

    Merge swagger

--- back-end/app.js
+++ back-end/app.js
@@ -1,37 +1,36 @@
-  const express = require('express');
-  const { connectDB } = require('./config/db');
-  const dotenv = require('dotenv');
-  const cors = require('cors');
+const express = require("express");
+const { connectDB } = require("./config/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-  // Cấu hình môi trường từ file .env
-  dotenv.config();
+// Cấu hình môi trường từ file .env
+dotenv.config();
 
-  const app = express();
+const app = express();
 
-  // Middleware để phân tích JSON
-  app.use(express.json());
+// Middleware để phân tích JSON
+app.use(express.json());
 
-  // Kích hoạt CORS
-  app.use(cors());
+// Kích hoạt CORS
+app.use(cors());
 
-  // Kết nối đến cơ sở dữ liệu SQL Server
-  connectDB();
+// Kết nối đến cơ sở dữ liệu SQL Server
+connectDB();
 
-  // Import các routes
-  const authRoutes = require('./routes/authRoutes');
-  const koiRoutes = require('./routes/koiRoutes');
-  const orderRoutes = require('./routes/orderRoutes');
-  const consignmentRoutes = require('./routes/consignmentRoutes.js');
+// Import các routes
+const authRoutes = require("./routes/authRoutes");
+const koiRoutes = require("./routes/koiRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const consignmentRoutes = require("./routes/consignmentRoutes.js");
 
-  // Sử dụng các routes với tiền tố /api
-  app.use('/api/auth', authRoutes);
-  app.use('/api/koi', koiRoutes);
-  app.use('/api/orders', orderRoutes);
-  app.use('/api/consignment', consignmentRoutes);
+// Sử dụng các routes với tiền tố /api
+app.use("/api/auth", authRoutes);
+app.use("/api/koi", koiRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/consignment", consignmentRoutes);
 
-
-  // Khởi động server
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running`);
-  });
+// Khởi động server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running`);
+});
