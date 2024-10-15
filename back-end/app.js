8ed084229dfb4789d89a7cfbae2869f1b54e12f6
hna-gnart <anhtntse183272@fpt.edu.vn>
2024-10-15 21:58:25 +0700

    9:58, push lại nha

--- back-end/app.js
+++ back-end/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Cấu hình CORS
 app.use(cors({
-    origin: '*',  // Cho phép frontend từ localhost:3000
+    origin: 'http://localhost:3000',  // Cho phép frontend từ localhost:3000
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Cho phép các phương thức này
     credentials: true,  // Cho phép gửi thông tin xác thực (cookie) nếu cần
 }));
