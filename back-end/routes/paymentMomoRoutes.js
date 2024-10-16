b11df4a6068540120e2789c00cfd112ac886d39f
Lê Nguyễn Đăng Khoa <khoalndse172734@fpt.edu.vn>
2024-10-16 23:18:51 +0700

    deleted unuse momo file

--- back-end/routes/paymentMomoRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentMomoController = require('../controllers/momoController');
-
-// Định nghĩa route POST cho tạo bill thanh toán
-router.post('/paymentgateway', paymentMomoController.paymentMomo)
